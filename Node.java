90285927c1579f83113cab75c92ec699b8afc711
Mervanaykut <100205258+Mervan34@users.noreply.github.com>
2022-12-28 19:48:07 +0300

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package agac;
+
+/**
+ *
+ * @author MERVAN
+ */
+public class Node {
+    Node left;
+    Node right;
+    int data;
+
+    public Node( int data) {
+        this.data = data;
+       
+    }
+
+    
+}
